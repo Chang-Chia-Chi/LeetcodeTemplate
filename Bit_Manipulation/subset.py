8e43d4bea2bb50db6574656f57c6ec5a42b8b3a0
Max Chang <maxchang0724@gmail.com>
2025-02-15 21:41:35 +0800

    feat: gospersHack

--- Bit_Manipulation/subset.py
+++ Bit_Manipulation/subset.py
@@ -3,3 +3,13 @@ def getSubsets(self, state):
     while subset:
         yield subset
         subset = (subset - 1) & state
+
+
+# Iterate all the m-bit state where there are k 1-bits.
+def gospersHack(self, m, k, state):
+    state = (1 << k) - 1
+    while state < (1 << m):
+        yield state
+        c = state & -state
+        r = state + c
+        state = (((r ^ state) >> 2) / c) | r
